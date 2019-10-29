b5c1f56914270292c4710171fa02a97fe6b39bdf
Franka van Jaarsveld <frankav@student.uia.no>
2019-10-29 14:56:58 +0100

    user post works

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors'); //when the clients aren't on the server
 const app = express(); //server-app
 const pg = require('pg');
-const dbURI = "postgres://pzofkizznodbem:34a5053ee54aeb364552c6e348f5743550e02d6750f112ecbfed0a608bddb319@ec2-46-51-190-87.eu-west-1.compute.amazonaws.com:5432/d86rrspbg64el6"
+const dbURI = "postgres://kqtgokmgwwiwlv:6eadab2bf3f619a6cbffdf551abd6d0469b1134ceda61ac432a9b090941bc610@ec2-54-75-235-28.eu-west-1.compute.amazonaws.com:5432/d9nba8lrkjpjeu"
     + "?ssl=true"; // for encryption
 const connString = process.env.DATABASE_URL || dbURI;
 const pool = new pg.Pool({connectionString: connString });
+const bcrypt = require('bcrypt');
+const secret = "frenchfriestastegood!"; // for tokens = should be store as an environment variable;
+const jwt = require('jsonwebtoken');
 
 // middleware ------------------------------------
 app.use(cors()); //allow all CORS requests
@@ -19,8 +22,8 @@ app.post("/users", async function(req, res, next) {
     // hash the pwd before it is stored in the db
     let hash = bcrypt.hashSync(updata.password, 10);
 
-    let sql = "INSERT INTO users (id, username, email, pwdhash, lists) VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *";
-    let values = [updata.username, updata.email, hash, null];
+    let sql = "INSERT INTO users (id, username, email, pwdhash) VALUES(DEFAULT, $1, $2, $3) RETURNING *";
+    let values = [updata.username, updata.email, hash];
 
     try {
         let result = await pool.query(sql, values);
