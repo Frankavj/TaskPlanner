f9368cfff550d5f0e159503be242fd33de3e5d53
Franka van Jaarsveld <frankav@student.uia.no>
2019-11-22 00:46:20 +0100

    modals look a lot better (responsive) also framework for adding and removing access done

--- client/indexJS/user.js
+++ client/indexJS/user.js
@@ -30,43 +30,113 @@ export async function getUserById(id) {
     return user;
 }
 
-// Show users with access --------------------------------------------------------------------------------
-let modal = document.getElementById("accessModal");
-let modalBody = document.getElementsByClassName("modal-body")[0];
-let span = document.getElementsByClassName("close")[0];
+// Show users with access - share with me -------------------------------------------------------------------
+let accessModal = document.getElementById("accessModal");
+let accessModalBody = document.getElementsByClassName("modal-body")[0];
+let accessModalSpan = document.getElementsByClassName("close")[0];
 let sharedWith = document.getElementById("sharedWith");
 
 sharedWith.addEventListener('click', async function () {
 
-    modal.style.display = "block";
+    accessModal.style.display = "block";
     let list = JSON.parse(localStorage.getItem("listinfo"));
+
+    accessModalBody.innerHTML = "" // remove previous content
 
     for (let id of list.individual_access) {
         let user = await getUserById(id);
-        
-        if(user.id != logindata.userid) {
+
+        if (user.id != logindata.userid) {
             let div = document.createElement('div');
+            div.setAttribute('class', 'theySharedWith');
 
-        let img = document.createElement('img');
-        img.setAttribute('src', "../img/boy_1.png"); //TODO
-        div.appendChild(img);
+            let img = document.createElement('img');
+            img.setAttribute('src', "../img/boy_1.png"); //TODO
+            div.appendChild(img);
 
-        let name = document.createElement('p');
-        name.innerHTML = user.username;
-        div.appendChild(name);
+            let name = document.createElement('p');
+            name.innerHTML = user.username;
+            div.appendChild(name);
 
-        modalBody.appendChild(div);
+            accessModalBody.appendChild(div);
         }
     }
 
 });
 
-span.addEventListener('click', function () {
-    modal.style.display = "none";
+accessModalSpan.addEventListener('click', function () {
+    accessModal.style.display = "none";
 });
 
 window.addEventListener('click', function () {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == accessModal) {
+        accessModal.style.display = "none";
+    }
+});
+
+// Show users with access - shared by me --------------------------------------------------------------------
+let shareModal = document.getElementById("shareModal");
+let shareModalBody = document.getElementsByClassName("modal-body")[1];
+let shareModalSpan = document.getElementsByClassName("close")[1];
+let searchUname = document.getElementById("searchUname");
+let searchEmail = document.getElementById("searchEmail");
+let btnSearch = document.getElementById("btnSearch");
+let have_accessTitle = document.getElementById("have_accessTitle");
+let have_access = document.getElementById("have_access");
+let share = document.getElementById("btnAddPeople");
+
+share.addEventListener('click', async function () {
+
+    shareModal.style.display = "block";
+
+    let list = JSON.parse(localStorage.getItem("listinfo"));
+
+    if (list.individual_access) {
+        have_accessTitle.style.display = "block";
+        have_access.style.display = "block";
+        have_access.innerHTML = ""; // remove previous content
+
+        for (let id of list.individual_access) {
+            let user = await getUserById(id);
+
+            if (user.id != logindata.userid) {
+                let div = document.createElement('div');
+                div.setAttribute('class', 'iSharedWith')
+
+                let greybox = document.createElement('div');
+                greybox.setAttribute('class', 'greybox');
+
+                let img = document.createElement('img');
+                img.setAttribute('src', "../img/boy_1.png"); //TODO
+                greybox.appendChild(img);
+
+                let name = document.createElement('p');
+                name.innerHTML = user.username;
+                greybox.appendChild(name);
+
+                div.appendChild(greybox);
+
+                let removeUser = document.createElement('img');
+                removeUser.setAttribute('src', '../img/trash.png');
+
+                div.appendChild(removeUser);
+
+                have_access.appendChild(div);
+            }
+        }
+    } else {
+        have_accessTitle.style.display = "none";
+        have_access.style.display = "none";
+    }
+
+});
+
+shareModalSpan.addEventListener('click', function () {
+    shareModal.style.display = "none";
+});
+
+window.addEventListener('click', function () {
+    if (event.target == shareModal) {
+        shareModal.style.display = "none";
     }
 });
